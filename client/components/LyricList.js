b82300e3e2f5122ebf3d941212191429c3109d7f
Wanwan <wanwanzainaer@gmail.com>
2019-09-15 22:45:14 -0700

    Add  optimistic ui update

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
 class LyricList extends Component {
-  onLike(id) {
-    console.log(id);
+  onLike(id, likes) {
+    this.props.mutate({
+      variables: { id },
+      optimisticResponse: {
+        __typename: "Mutation",
+        likeLyric: {
+          id: id,
+          __typename: "LyricType",
+          likes: likes + 1
+        }
+      }
+    });
   }
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
-          <i
-            className="material-icons"
-            onClick={() => this.onLike(id)}
-          >
-            thumb_up
-          </i>
+          <div className="vote-box">
+            <i
+              className="material-icons"
+              onClick={() => this.onLike(id, likes)}
+            >
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       );
     });
@@ -23,5 +38,13 @@ class LyricList extends Component {
     return <ul className="collection">{this.renderLyrics()}</ul>;
   }
 }
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
 
-export default LyricList;
+export default graphql(mutation)(LyricList);
